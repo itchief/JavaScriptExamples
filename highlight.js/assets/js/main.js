17d1420383aa57874c5767c5e2af3e8e21cf5786
itchief <alex.malcev1980@gmail.com>
2021-10-09 22:19:00 +1000

    Update

---
+++ highlight.js/assets/js/main.js
@@ -0,0 +1,57 @@
+(function() {
+  let state = false;
+  const ver = document.documentElement.dataset.ver;
+  const elements = [];
+  // функция highlight
+  function highlight() {
+    const items = elements.slice();
+    elements.length = 0;
+    items.forEach(item => {
+      let lang = false;
+      item.className.split(' ').forEach(item => {
+        if (item.indexOf('lang-') === 0) {
+          lang = item.replace('lang-', '');
+        }
+      })
+      const result = lang ? hljs.highlight(item.textContent, { language: lang }) : hljs.highlightAuto(item.textContent);
+      item.innerHTML = result.value;
+    })
+  }
+  function loadCSSandJS() {
+    // вставляем скрипт
+    const script = document.createElement('script');
+    script.src = 'assets/js/highlight.min.js';
+    script.async = 1;
+    document.head.appendChild(script);
+    script.onload = function() {
+      state = true;
+      highlight();
+    }
+    // вставляем стили
+    const style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = 'assets/css/highlight.min.css';
+    document.head.appendChild(style);
+  }
+  // создаем observer
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const target = entry.target;
+        elements.push(target);
+        if (state) {
+          highlight();
+        } else {
+          loadCSSandJS();
+        }
+        observer.unobserve(target);
+      }
+    });
+  }, {
+    root: null,
+    rootMargin: '0px 0px 200px 0px'
+  });
+  document.querySelectorAll('.prettyprint, .hljs').forEach(item => {
+    observer.observe(item);
+  });
+})();
